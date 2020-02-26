85b169d24e6b5d331bea8ceb986242d5b491e6fb
Titax123 <61444501+Titax123@users.noreply.github.com>
2020-02-26 10:32:41 +0500

    Add files via upload

---
+++ ZAk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZAk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
